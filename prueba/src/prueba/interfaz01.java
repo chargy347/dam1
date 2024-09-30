c7da23cb3dfd659e0d10f5d1cd275172b3e9525f
Usuario <Usuario@192.168.13.50>
2024-09-30 09:32:22 +0200

    interfaz 01

--- prueba/src/prueba/interfaz01.java
+++ prueba/src/prueba/interfaz01.java
@@ -6,11 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
 
 public class interfaz01 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,27 @@ public class interfaz01 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(23, 59, 96, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(23, 105, 96, 20);
+		contentPane.add(passwordField);
+		
+		JButton btnNewButton = new JButton("aceptar");
+		btnNewButton.setBounds(139, 162, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Contraseña");
+		lblNewLabel.setBounds(23, 90, 75, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		lblNewLabel_1.setBounds(23, 44, 49, 14);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
