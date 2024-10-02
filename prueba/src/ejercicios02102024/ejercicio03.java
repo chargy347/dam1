66b4c59bf1ae05d3ac85765b97243e62c00c6380
juanv <juanv@DESKTOP-G3BDD37>
2024-10-02 17:48:03 +0200

    x

---
+++ prueba/src/ejercicios02102024/ejercicio03.java
@@ -0,0 +1,10 @@
+package ejercicios02102024;
+
+public class ejercicio03 {
+	public static void main (String [] agrs) {
+		int altura = 2;
+		int base = 2;
+		int area = base * altura;
+		System.out.println("el area es: " + altura);
+	}
+}
---
+++ prueba/src/ejercicios02102024/ejercicio04.java
@@ -0,0 +1,9 @@
+package ejercicios02102024;
+
+public class ejercicio04 {
+	public static void main (String[] args) {
+		int cel = 35;
+		int fah = (cel * 9/5) +32;
+		System.out.println(cel + " grados Celsius son " + fah + " grados Fahrenheit.");
+	}
+}
