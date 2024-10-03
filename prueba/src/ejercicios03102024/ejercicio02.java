eeac7327a62b4398d36206685c6f5af39be53a62
Usuario <Usuario@192.168.13.50>
2024-10-03 13:07:33 +0200

    ejercicios

---
+++ prueba/src/ejercicios03102024/ejercicio02.java
@@ -0,0 +1,16 @@
+package ejercicios03102024;
+import java.util.Scanner;
+
+public class ejercicio02 {
+	public static void main (String[] agrs) {
+        Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingresa un numero");
+        int n1 = scanner.nextInt();
+		System.out.println("Ingresa un numero2");
+        int n2 = scanner.nextInt();
+		System.out.println("Ingresa un numero3");
+        int n3 = scanner.nextInt();
+        
+        
+	}
+}
